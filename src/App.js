1b82f2a0ac44d29baaac32795b80d24fe469125b
Brian Ly <brian.kyang.ly@gmail.com>
2019-10-30 17:06:17 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -1,62 +1,53 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Timer from './Timer'
+import Timer from "./Timer";
 
 class App extends Component {
-
   //no props being used here, so we can use the shorthand declaration of state
   state = {
     timerIDs: []
-  }
-
+  };
 
   //Your code here:
 
-
-
-
-
-
-
-
+  componentDidMount() {
+    this.handleAddTimer();
+  }
 
   // No need to modify anything in render or the class methods below
   // Unless, of course, you're curious about how it all works
   render() {
-
     return (
       <div className="App">
         <h1>MultiTimer</h1>
         <button onClick={this.handleAddTimer}>Add New Timer</button>
 
-        <div className="TimerGrid">
-          {this.renderTimers()}
-        </div>
-
+        <div className="TimerGrid">{this.renderTimers()}</div>
       </div>
     );
   }
 
   // returns array of components written in JSX, mapped from this.state.timerIDs
-  renderTimers = () => this.state.timerIDs.map(id => {
-    return <Timer key={id} id={id} removeTimer={this.removeTimer} />
-  })
+  // renders Timer components with a randomly generated key
+  renderTimers = () =>
+    this.state.timerIDs.map(id => {
+      return <Timer key={id} id={id} removeTimer={this.removeTimer} />;
+    });
 
   // adds a random number for timer ID
   handleAddTimer = () => {
     this.setState(prevState => ({
-      timerIDs: [...prevState.timerIDs, Math.floor(Math.random()*1000)]
-    }))
-  }
+      timerIDs: [...prevState.timerIDs, Math.floor(Math.random() * 1000)]
+    }));
+  };
 
   // removeTimer updates state, removing any timer that matches the provided author
+  // returns an array of all ids that dont match the id being passed in
   removeTimer = id => {
     this.setState(prevState => ({
       timerIDs: prevState.timerIDs.filter(timer_id => timer_id !== id)
-    }))
-  }
-
-
+    }));
+  };
 }
 
 export default App;
